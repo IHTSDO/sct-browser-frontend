1b0f79af29ed6a13cddb42dbcbfb1242e486d81d
vijesh <santhanavijesh@gmail.com>
2024-02-09 19:18:03 +0530

    BROWSE-682 - Implemented ECL

---
+++ cypress/e2e/C605828-ECL.cy.js
@@ -0,0 +1,105 @@
+import { it } from "mocha"
+import AcceptCookiesDisclaimerPage from "../pages/AcceptCookiesDisclaimerPage"
+import ECLPage from "../pages/ECLPage"
+
+
+const accept = new AcceptCookiesDisclaimerPage()
+const eclquey = new ECLPage()
+
+describe("C605828-ECL", () => {
+    it("Launch browser url", () => {
+     accept.visit()
+    })
+
+// Click on Accept cookies  
+it("click on Accept cookies", () => {
+    accept.getCookiesAccept().click()
+  })
+
+// Click on Accept disclaimers 
+it("click on Accept disclaimers", () => {
+    accept.getDisclaimersAccept().click()
+   })
+  
+// Select InternationalEdition
+it("select International Edition", () => {
+    accept.getInternationEdition().click()
+})
+  
+//Select Expression Constraint Queries tab on the right
+it("navigate to ECL query page", () => {
+    eclquey.getExpressionConstraintTab()
+})
+
+//Click on ECL Builder button
+it("click on ECL builder button", () => {
+    eclquey.getEclBuilderButton()
+})
+
+//Search for '80891009' and select the match
+it("search concept 80891009", () => {
+    cy.fixture('ECLQuery').then((data) => {
+        eclquey.getSearchConcept(data.conceptnumber)
+    })
+})
+
+//Click on Ok button
+it("click on OK button", () => {
+    eclquey.getOkButton()
+})
+
+//Scroll the page
+it("Scroll page", () => {
+    cy.get('#fh-tabs-pane2').scrollTo('top')
+  })
+
+//Click on Execute button
+it("click on Execute button", () => {
+    eclquey.getECLQuery()
+    eclquey.getExecuteButton()
+})
+
+//Verify the query execution result
+it("check query execution and result", () => {
+    eclquey.getResult()
+})
+
+//Click on ECL Builder button
+it("click on ECL builder button", () => {
+    eclquey.getEclBuilderButton()
+})
+
+//Click on Add Refinement button
+it("click on Add Refinement button", () => {
+    eclquey.getAddRefinementButton()
+})
+
+//Populate refinement with Laterality (attribute) and Side (qualifier value)
+it("populate attribute and qualifier values", () => {
+    cy.fixture('ECLQuery').then((data) => {
+       eclquey.getPopulateRefinementValues(data.attribute, data.qualifier)
+    })
+})
+
+//Click on Ok button
+it("click on OK button", () => {
+    eclquey.getOkButton()
+})
+
+//Scroll the page
+it("Scroll page", () => {
+    cy.get('#fh-tabs-pane2').scrollTo('top')
+  })
+
+//Click on Execute button
+it("click on Execute button", () => {
+    eclquey.getECLQuery()
+    eclquey.getExecuteButton()
+})
+
+//Verify the query execution result
+it("check query execution and result", () => {
+    eclquey.getResult()
+})
+
+})
---
+++ cypress/pages/ECLPage.js
@@ -0,0 +1,91 @@
+class ECLPage {
+
+// Select Expression Constraint Queries tab on the right
+getExpressionConstraintTab(){
+    cy.get('a[href="#fh-query_canvas"]').click()
+    if(cy.get('.col-md-6 > .lead').should('have.text', 'Enter an ECL query'))
+      {
+         return cy.log('Expression Constraint Queries page is displayed');
+      }
+ }
+ 
+// Click on ECL Builder button
+getEclBuilderButton(){
+    cy.get('button[id="-eclBuilderButton"]').click()
+    if(cy.get('.modal-header > h3').should('have.text','ECL Builder'))
+    {
+      return cy.log('ECL builder page is displayed')
+    }
+}
+
+// Search for '80891009' and select the match
+getSearchConcept(conceptnumber){
+    //Enter the concept number in searchbox
+    cy.get('.ecl-builder-typeahead > .ng-untouched').type(conceptnumber)
+    //Select the concept
+    cy.get('ngb-highlight').click()
+    if(cy.get('.output > :nth-child(1)').should('contain', '<< 80891009 |Heart structure (body structure)|'))
+     {
+       return cy.log('Heart Structure concept is selected')
+     }
+}
+
+//Click on Ok button
+getOkButton(){
+    cy.get('.accept') .click()
+    if(cy.get('.col-md-6 > .lead').should('have.text', 'Enter an ECL query'))
+    {
+      return cy.log('ECL tab page is displayed');
+    }
+}
+
+//Verify ECL query is added
+getECLQuery(){
+    cy.get('#fh-query_canvas-ExpText').then(($ele) => {
+      expect($ele.text()).to.not.be.null
+      return cy.log('ECL query is added')
+  }) 
+}
+
+// Click on Execute button
+getExecuteButton(){
+    return cy.get('#fh-query_canvas-computeInferredButton2').click()
+}
+
+// Get Result
+getResult(){
+  if(cy.get('#fh-query_canvas-outputBody', { timeout: 10000 }).should('be.visible'))
+    {
+       return cy.log('Query executed and result is displayed')
+    }
+}
+
+// Click on Add Refinment button
+getAddRefinementButton(){
+    cy.contains('ADD REFINEMENT').click()
+    if(cy.get('.green > span').should('be.visible'))
+    {
+       return cy.log('Add refinement is displayed')
+    }
+}
+
+//Populate refinement with Laterality (attribute) and Side (qualifier value)
+getPopulateRefinementValues(attribute, qualifier){
+    //Enter Laterality in search textbox
+    cy.get(':nth-child(3) > .expression-group > .expression-row > :nth-child(2) > .ecl-builder-typeahead > .ng-untouched').type(attribute)
+    //Select the Laterality value
+    cy.get('ngb-highlight').click()
+
+    //Enter Side in search textbox
+    cy.get(':nth-child(5) > .ecl-builder-typeahead > .ng-untouched').type(qualifier)
+    //Select Side value
+    cy.get('#ngb-typeahead-4-0 > ngb-highlight').click()
+
+    if(cy.get('.output').should('contain', '<< 272741003 |Laterality (attribute)| = << 182353008 |Side (qualifier value)|'))
+    {
+      return cy.log('Refinement with Laterality (attribute) and Side (qualifier value) populated')
+    }
+}
+}
+
+module.exports = ECLPage
